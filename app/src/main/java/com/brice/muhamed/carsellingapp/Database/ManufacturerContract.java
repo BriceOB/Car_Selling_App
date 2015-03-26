4cd38331c61e8b5b820a8a044d1cd5055024d8b7
muhamedRedzic <muhamed.r@netplus.ch>
2015-03-26 10:08:53 +0100

    Muhamed

--- app/src/main/java/com/brice/muhamed/carsellingapp/Database/ManufacturerContract.java
+++ app/src/main/java/com/brice/muhamed/carsellingapp/Database/ManufacturerContract.java
@@ -1,7 +1,36 @@
 package com.brice.muhamed.carsellingapp.Database;
 
+import android.provider.BaseColumns;
+import android.util.Log;
+
 /**
- * Created by Muhamed on 26.03.2015.
+ * Created by Brice on 12/03/2015.
  */
-public class ManufacturerContract {
+public final class ManufacturerContract {
+    // To prevent someone from accidentally instantiating the contract class,
+// give it    an empty constructor .
+    public ManufacturerContract() {
+
+
+    }
+
+
+
+    public static abstract class EntryManufacturer implements BaseColumns {
+        public static final String TABLE_NAME       = "Manufacturer";
+        public static final String COLUMN_NAME_BRAND = "Brand";
+
+        private static final String TEXT_TYPE          = " TEXT";
+        private static final String COMMA_SEP          = ",";
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_BRAND + TEXT_TYPE  + " )";
+
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+
+
+
 }
--- app/src/main/java/com/brice/muhamed/carsellingapp/Object/Car.java
+++ app/src/main/java/com/brice/muhamed/carsellingapp/Object/Car.java
@@ -1,7 +1,129 @@
 package com.brice.muhamed.carsellingapp.Object;
 
+import java.util.Date;
+
 /**
- * Created by Muhamed on 26.03.2015.
+ * Created by Brice on 25/03/2015.
  */
 public class Car {
+
+    private int Id;
+    private   String Model ;
+    private   int Kilometers;
+    private   String Fuel ;
+    private   int Doors ;
+    //  private   int Hp ;
+    // private   int Weight ;
+    private   int Price;
+    private   String Description ;
+    private   String PhotoPath ;
+    private   String CreationDate ;
+    private   String CarDate ;
+    private   int ToSell ;
+    public    int ManufacturerId ;
+    public    int SellerId;
+
+    public Car( String model, int kilometers, String fuel, int doors,
+                //  int hp,int weight,
+                int price, String description,
+                String creationDate, String carDate, int toSell, int manufacturerId,
+                int sellerId) {
+
+
+        Model = model;
+        Kilometers = kilometers;
+        Fuel = fuel;
+        Doors = doors;
+        //   Hp = hp;
+        //    Weight = weight;
+        Price = price;
+        Description = description;
+
+        CreationDate = creationDate;
+        CarDate = carDate;
+        ToSell = toSell;
+        ManufacturerId = manufacturerId;
+        SellerId = sellerId;
+    }
+
+
+
+    public void setPhotoPath(String photoPath){
+
+        PhotoPath = photoPath;
+
+    }
+
+    public void setId(int id){
+
+        Id = id;
+
+    }
+
+
+
+    /*GETTERS
+
+     */
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public int getKilometers() {
+        return Kilometers;
+    }
+
+    public String getFuel() {
+        return Fuel;
+    }
+
+    public int getDoors() {
+        return Doors;
+    }
+
+    /*
+    public int getHp() {
+        return Hp;
+    }
+
+    public int getWeight() {
+        return Weight;
+    }
+*/
+    public int getPrice() {
+        return Price;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getPhotoPath() {
+        return PhotoPath;
+    }
+    /*
+        public Date getCreationDate() {
+            return CreationDate;
+        }
+
+        public Date getCarDate() {
+            return CarDate;
+        }
+    */
+    public int getToSell() {
+        return ToSell;
+    }
+
+    public int getManufacturerId() {
+        return ManufacturerId;
+    }
+
+    public int getSellerId() {
+        return SellerId;
+    }
 }
