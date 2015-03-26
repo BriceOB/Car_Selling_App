19b2e9fef7a95572e60d5bfd00a961bea97ff5af
muhamedRedzic <muhamed.r@netplus.ch>
2015-03-26 10:05:08 +0100

    Muhamed

---
+++ app/src/main/java/com/brice/muhamed/carsellingapp/Database/ManufacturerContract.java
@@ -0,0 +1,7 @@
+package com.brice.muhamed.carsellingapp.Database;
+
+/**
+ * Created by Muhamed on 26.03.2015.
+ */
+public class ManufacturerContract {
+}
---
+++ app/src/main/java/com/brice/muhamed/carsellingapp/Object/Car.java
@@ -0,0 +1,7 @@
+package com.brice.muhamed.carsellingapp.Object;
+
+/**
+ * Created by Muhamed on 26.03.2015.
+ */
+public class Car {
+}
