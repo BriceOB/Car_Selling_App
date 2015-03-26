3e9b7f102d0552d1723b7a6faff86901f20e50e1
muhamedRedzic <muhamed.r@netplus.ch>
2015-03-26 09:30:20 +0100

    Muhamed

--- app/src/main/java/com/brice/muhamed/carsellingapp/About.java
+++ app/src/main/java/com/brice/muhamed/carsellingapp/About.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class About extends ActionBarActivity {
 
+    //About
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
