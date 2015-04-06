6839ddb15d72a1fc7889e00ea7805c440af6f88e
BriceOB <Briceolivier.b@gmail.com>
2015-04-06 16:22:01 +0200

    Brice

--- app/src/main/java/com/brice/muhamed/carsellingapp/InsertCarDetails.java
+++ app/src/main/java/com/brice/muhamed/carsellingapp/InsertCarDetails.java
@@ -1,9 +1,16 @@
 package com.brice.muhamed.carsellingapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -18,6 +25,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -25,6 +33,11 @@ import android.widget.Toast;
 import com.brice.muhamed.carsellingapp.Database.DatabaseHandler;
 import com.brice.muhamed.carsellingapp.Object.Car;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -185,6 +198,114 @@ public class InsertCarDetails extends ActionBarActivity {
         toast.show();
 
         this.finish();
+    }
+
+    public void UploadCarImage(View view) {
+        selectImage();
+    }
+
+    private void selectImage() {
+        final CharSequence[] options = getResources().getStringArray(R.array.PopUpIMage);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(InsertCarDetails.this);
+        builder.setTitle(getResources().getString(R.string.AddPhoto));
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take a photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }else if(options[item].equals("Take from gallery") || options[item].equals("Von Gallerie auswählen") || options[item].equals("Choisir de la Gallerie")) {
+//                    Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                    intent.setType("image/*");
+//                    startActivityForResult(Intent.createChooser(intent, "Select File"),2);
+
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+//
+//                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+//                    photoPickerIntent.setType("image/*");
+//                    startActivityForResult(photoPickerIntent, 2);
+
+                } else {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+
+
+                    File path = new File(android.os.Environment.getExternalStorageDirectory() + File.separator + "CarImages" + File.separator);
+                    path.mkdirs();
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+
+                        Bitmap thumbnail = (BitmapFactory.decodeFile(file.getAbsolutePath()));
+                        ((ImageView)findViewById(R.id.imageViewInsertCarDetails)).setImageBitmap(thumbnail);
+                        ImageView imageView = (ImageView)findViewById(R.id.imageViewInsertCarDetails);
+                        int width = 960;
+                        int height = thumbnail.getHeight() * width / thumbnail.getWidth();
+                        imageView.setImageBitmap(Bitmap.createScaledBitmap(thumbnail, width, height, false));
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                ImageView imageView = (ImageView)findViewById(R.id.imageViewInsertCarDetails);
+                int width = 960;
+                int height = thumbnail.getHeight() * width / thumbnail.getWidth();
+                imageView.setImageBitmap(Bitmap.createScaledBitmap(thumbnail, width, height, false));
+            }
+        }
     }
 
 
